6210f35eaf816c9de115c0bc8f1626745ebe0ad6
Andrazx23 <andrazx37@gmail.com>
2026-04-18 22:57:53 +0700

    Update ui.lua

--- flow ui/ui.lua
+++ flow ui/ui.lua
@@ -81,6 +81,11 @@ local vora_logo     = getcustomasset("vora_logo.png")
 local vora_ui = {}
 vora_ui.__index = vora_ui
 
+local Toggles = {}
+local Options = {}
+_G.Toggles = Toggles
+_G.Options = Options
+
 local local_player      = players.LocalPlayer
 local player_mouse      = local_player:GetMouse()
 
@@ -5197,6 +5202,7 @@ function vora_ui:AddSection(config)
                 
                 function toggleObj:Set(value, silent)
                     toggleObj.value = value == true
+                    toggleObj.Value = toggleObj.value
                     if toggleObj.value then
                         tween_to(toggleObj.switchFrame, {BackgroundColor3 = groupObj.Library.config.AccentColor}, 0.2)
                         tween_to(toggleObj.circleFrame, {Position = UDim2.new(0.462, 0, 0.143, 0), BackgroundColor3 = Color3.new(1, 1, 1)}, 0.2, Enum.EasingStyle.Back, Enum.EasingDirection.Out)
@@ -5223,6 +5229,10 @@ function vora_ui:AddSection(config)
                 end, function(value)
                     toggleObj:Set(value == true, true)
                 end)
+                
+                -- Store in global Toggles table
+                Toggles[toggleConfig.Name] = toggleObj
+                toggleObj.Value = toggleObj.value
                 
                 groupObj.element_y = groupObj.element_y + 28 * scale_factor
                 update_group_size()
@@ -5362,6 +5372,7 @@ function vora_ui:AddSection(config)
                 function sliderObj:Set(value, instant, silent)
                     value = normalize_slider_value(value, sliderConfig.Min, sliderConfig.Max, sliderConfig.Increment, sliderPrecision)
                     sliderObj.value = value
+                    sliderObj.Value = sliderObj.value
                     updateSliderVisuals(instant ~= true)
                     if not silent then
                         sliderConfig.Callback(value)
@@ -5414,6 +5425,11 @@ function vora_ui:AddSection(config)
                 end, function(value)
                     sliderObj:Set(tonumber(value) or sliderConfig.Min, true, true)
                 end)
+                
+                -- Store in global Options table
+                Options[sliderConfig.Name] = sliderObj
+                sliderObj.Value = sliderObj.value
+                
                 groupObj.element_y = groupObj.element_y + 44 * scale_factor
                 update_group_size()
                 table.insert(groupObj.elements, sliderObj)
@@ -6282,6 +6298,7 @@ function vora_ui:AddSection(config)
                         })
                         optionClickButton.MouseButton1Click:Connect(function()
                             dropdownObj.value = option
+                            dropdownObj.Value = dropdownObj.value
                             closeDropdown(false)
                             local optionText = tostring(option)
                             dropdownObj.selectedLabelText.Text = optionText
@@ -6362,6 +6379,7 @@ function vora_ui:AddSection(config)
                 function dropdownObj:Set(value, silent)
                     if value == nil then return end
                     dropdownObj.value = value
+                    dropdownObj.Value = dropdownObj.value
                     local displayValue = tostring(value)
                     dropdownObj.selectedLabelText.Text = displayValue
                     local newWidth = math.max(70 * scale_factor, measure_text_width(displayValue, 12 * scale_factor) + 30 * scale_factor)
@@ -6438,6 +6456,10 @@ function vora_ui:AddSection(config)
                         dropdownObj:Set(value, true)
                     end
                 end)
+                
+                -- Store in global Options table
+                Options[dropdownConfig.Name] = dropdownObj
+                dropdownObj.Value = dropdownObj.value
                 
                 groupObj.element_y = groupObj.element_y + 28 * scale_factor
                 update_group_size()
